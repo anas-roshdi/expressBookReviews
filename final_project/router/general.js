14016bbc911bd56c961000fd0a36ac36e4c3c6b6
anas-roshdi <a7nas.talika@gmail.com>
2026-01-08 08:43:40 -0500

    Task 5-7

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,9 +5,19 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;  
+  const password = req.body.password;
+  if (username && password) {
+    const exists = users.filter((user) => user.username === username);
+    if (exists.length === 0) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Unable to register user: Username and/or Password not provided." });
 });
 
 // Get the book list available in the shop
@@ -76,9 +86,14 @@ public_users.get('/title/:title',function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn].reviews);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
+
 
 module.exports.general = public_users;
